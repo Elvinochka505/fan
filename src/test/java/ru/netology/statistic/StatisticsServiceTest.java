d9bf57c7d9c0dd60bd9a6963ea888a44748a49c8
Elvina <khabibullinaelvina@mail.ru>
2025-04-28 09:43:51 +0500

    jacoco setup

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,11 +7,23 @@ import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMax1() {
         ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax2() {
+        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 17, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 17;
 
         long actual = service.findMax(incomesInBillions);
 
